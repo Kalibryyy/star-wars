8bc8f519efa1e945cf0474c2ab466715488712f1
PavelYakupov <pyakupov@broadinstitute.org>
2021-05-21 16:38:54 +0300

    fix want-to-visit

--- src/app/want-to-visit/want-to-visit.component.ts
+++ src/app/want-to-visit/want-to-visit.component.ts
@@ -12,19 +12,20 @@ export class WantToVisitComponent implements DoCheck {
   wishList: any[] = [];
 
   ngDoCheck() {
-    if (this.wishPlanet) {
+    if (this.wishPlanet && !this.wishList.includes(this.wishPlanet)) {
       if (this.wishList.length) {
         this.wishPlanet = ', ' + this.wishPlanet;
       }
       this.wishList.push(this.wishPlanet);
       this.wishPlanet = '';
+    } else if (this.noMoreWishPlanet) {
+      this.wishList = this.wishList.filter(planet => {
+        if (planet.includes(', ')) {
+          planet = planet.slice(2)
+        }
+        return planet !== this.noMoreWishPlanet
+      });
     }
-    this.wishList = this.wishList.filter(planet => {
-      if (planet.includes(', ')) {
-        planet = planet.slice(2)
-      }
-      return planet !== this.noMoreWishPlanet
-    });
     if (this.wishList.length && this.wishList[0].includes(', ')) {
       this.wishList[0] = this.wishList[0].slice(2)
     }
