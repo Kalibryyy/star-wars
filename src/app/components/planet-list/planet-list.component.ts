72d6c641bfab3283e08ff197a9361cec6a893cc9
Yelena Strizhakova <e.strizhakova@gmail.com>
2021-06-22 14:49:56 +0300

    fetch planets

--- src/app/components/planet-list/planet-list.component.ts
+++ src/app/components/planet-list/planet-list.component.ts
@@ -1,8 +1,7 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {PlanetsService} from "../../services/planets.service";
 import {Planet} from "../../models/planet-model";
-import {catchError} from "rxjs/operators";
-import {throwError} from "rxjs";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-planet-list',
@@ -13,13 +12,16 @@ export class PlanetListComponent implements OnInit {
   @Output() wishPlanet: EventEmitter<string> = new EventEmitter<string>();
   @Output() noMoreWishPlanet: EventEmitter<string> = new EventEmitter<string>();
 
-  planets: Planet[] = [];
   loading = false;
-  errorMessage = '';
-  // error$: any;
-  // planets$: any;
 
-  constructor(private readonly planetsService: PlanetsService) {
+  error$: Observable<string>;
+  planets$: Observable<Planet[]>;
+
+  constructor(
+    private readonly planetsService: PlanetsService
+  ) {
+    this.error$ = this.planetsService.error$;
+    this.planets$ = this.planetsService.planets$;
   }
 
   ngOnInit() {
@@ -27,21 +29,21 @@ export class PlanetListComponent implements OnInit {
     // this.error$ = this.planetsService.error$;
     // console.log(this.error$)
     // this.planets$ = this.planetsService.getPlanets();
-    this.planetsService.getPlanets()
-      .pipe(
-        catchError(err => {
-          if (err.status === 404) {
-            this.errorMessage = 'Запрашиваемый ресурс не найден'
-          } else {
-            this.errorMessage = err.message;
-          }
-          return throwError(err); // ?
-        })
-      )
-      .subscribe((planets: Planet[]) => {
-          this.planets = planets;
-        },
-      )
+    // this.planetsService.getPlanets()
+      // .pipe(
+      //   catchError(err => {
+      //     if (err.status === 404) {
+      //       this.errorMessage = 'Запрашиваемый ресурс не найден'
+      //     } else {
+      //       this.errorMessage = err.message;
+      //     }
+      //     return throwError(err); // ?
+      //   })
+      // )
+      // .subscribe((planets: Planet[]) => {
+      //     this.planets = planets;
+      //   },
+      // )
     this.loading = false;
   }
 
--- src/app/services/planets.service.ts
+++ src/app/services/planets.service.ts
@@ -1,16 +1,44 @@
 import {Injectable} from "@angular/core";
 import {HttpClient, HttpErrorResponse} from "@angular/common/http";
-import {Observable, of, Subject, throwError} from "rxjs";
+import {BehaviorSubject, Observable, of, Subject, throwError} from "rxjs";
 import {Planet} from "../models/planet-model";
-import {catchError, map} from "rxjs/operators";
+import {catchError, first, map, tap} from "rxjs/operators";
+import {environment} from "../../environments/environment";
 
 @Injectable({providedIn: 'root'})
 export class PlanetsService {
 
-  error$: Subject<HttpErrorResponse> = new Subject<HttpErrorResponse>();
+  error$: Subject<string> = new Subject<string>();
+  planets$: BehaviorSubject<Planet[]> = new BehaviorSubject<Planet[]>([]);
 
-  constructor(private readonly http: HttpClient) {
+  // error$: Subject<HttpErrorResponse> = new Subject<HttpErrorResponse>();
+
+  constructor(
+    private readonly http: HttpClient
+  ) {
+    this.fetchPlanets();
   }
+
+  fetchPlanets(): void {
+    this.http.get<{ results: Planet[] }>(`${environment.swapiServiceURL}/planets1`).pipe(
+      first(),
+      tap((planetList: { results: Planet[] }) => this.planets$.next(planetList.results)),
+      catchError((err: HttpErrorResponse) => {
+        if (err.status === 404) {
+          this.error$.next('Запрашиваемый ресурс не найден')
+        } else {
+          this.error$.next(err.message);
+        }
+        return of(err);
+      })
+    ).subscribe();
+  }
+
+  // getPlanets(): Observable<Planet[]>  {
+  //   return this.http.get<{ results: Planet[] }>(`${environment.swapiServiceURL}/planets1/`).pipe(
+  //     map(({ results }: { results: Planet[] }) => results),
+  //   );
+  // }
 
   // getPlanets(): Observable<Planet[]> {
   //   return this.http.get<{ results: Planet[] }>('https://swapi.dev/api/planets1/')
@@ -19,14 +47,14 @@ export class PlanetsService {
   //     )
   // }
 
-  getPlanets(): Observable<Planet[] | HttpErrorResponse>  {
-    return this.http.get<{ results: Planet[] }>('https://swapi.dev/api/planets1/')
-      .pipe(
-        map((planetList: { results: Planet[] }) => planetList.results),
-        catchError((err: HttpErrorResponse) => {
-          this.error$.next(err);
-          return of(err);
-        })
-      );
-  }
+  // getPlanets(): Observable<Planet[] | HttpErrorResponse>  {
+  //   return this.http.get<{ results: Planet[] }>('https://swapi.dev/api/planets1/')
+  //     .pipe(
+  //       map((planetList: { results: Planet[] }) => planetList.results),
+  //       catchError((err: HttpErrorResponse) => {
+  //         this.error$.next(err);
+  //         return of(err);
+  //       })
+  //     );
+  // }
 }
